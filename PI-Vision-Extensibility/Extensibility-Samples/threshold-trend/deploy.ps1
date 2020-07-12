f85fc2ab059cda6c0d23f3e5a54715b06f85ee30
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2020-07-13 05:48:59 +0900

    Added some docs for the deploy script

--- PI-Vision-Extensibility/Extensibility-Samples/threshold-trend/deploy.ps1
+++ PI-Vision-Extensibility/Extensibility-Samples/threshold-trend/deploy.ps1
@@ -1,11 +1,10 @@
-$PSScriptRoot
-Write-Host $PSScriptRoot
+# Moves the files to the PI Vision extensibility folder
 
 $pihome = $env:PIHOME64
 $symbolFolder = $pihome + "PIVision\Scripts\app\editor\symbols\ext"
 
 if (-Not (Test-Path $symbolFolder)) {
-    Write-Host "Not on the PI Vision server"
+    Write-Host "Not on the PI Vision server or ext folder is missing"
     Read-Host 
     exit
 }
@@ -15,5 +14,5 @@ $files = @("sym-thresholdtrend-config.html", "sym-thresholdtrend.css", "sym-thre
 foreach ($file in $files) {
     $source = $PSScriptRoot + "\" + $file
     $target = $symbolFolder
-    copy $source $target
+    Copy-Item $source $target
 }
